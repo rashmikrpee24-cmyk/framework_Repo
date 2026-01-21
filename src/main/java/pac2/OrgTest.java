04238341cdbe19e61e7f8b29115d79eb27807010
ranga <ranga@LAPTOP-HD3JIIRU>
2026-01-21 15:12:56 +0530

    ADD the maven

---
+++ src/main/java/pac2/OrgTest.java
@@ -0,0 +1,13 @@
+package pac2;
+
+import org.junit.Test;
+
+public class OrgTest {
+	@Test
+		public void createOrgTest() {
+		System.out.println("execute createOrgTest");
+		}
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute modifycontactTest");
+	}}
